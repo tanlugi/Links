c32f1579f028102af8a4bbdd294316aca480cfd7
Tanlugi <santosoliveira1643@gmail.com>
2024-01-29 21:29:41 -0300

    Trying to solve a bug.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function Toggle() {
   const html = document.documentElement
   html.classList.toggle("light")
 
-  const imagem = document.querySelector("#profile img")
+  let imagem = document.querySelector("#profile img")
   if (html.classList.contains("light")) {
-    imagem.setAttribute("src", "assets/avatarLight.jpg")
+    imagem.setAttribute("src", "assets/avatarLight")
   } else {
     imagem.setAttribute("src", "assets/avatarDark.jpg")
   }
